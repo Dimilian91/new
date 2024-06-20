be24e8fc936f4276aae647d9e4f795808a1633a1
Vlasov Dmitry <dimilian91@mail.ru>
2024-06-20 09:22:24 +0300

    add data at state

--- src/state.jsx
+++ src/state.jsx
@@ -9,11 +9,35 @@ const user = {
 };
 
 const users = {
-  0: {name: "Никита", lastname: "Никитин"},
-  1: {name: "Вася", lastname: "Васин"},
-  2: {name: "Настя", lastname: "Настина"},
-  3: {name: "Света", lastname: "Светина"},
-}
+  0: {
+    name: "Никита", 
+    lastname: "Никитин", 
+    email: "nikita@mail.ru", 
+    id: 2, 
+    avatar: "https://img.freepik.com/free-psd/3d-illustration-person-with-sunglasses_23-2149436178.jpg", 
+    about: "Тут мой рассказ о себе",},
+  1: {
+    name: "Вася", 
+    lastname: "Васин", 
+    email: "vacya@mail.ru", 
+    id: 3, 
+    avatar: "https://img.freepik.com/free-psd/3d-illustration-person-with-sunglasses_23-2149436178.jpg", 
+    about: "Тут мой рассказ о себе",},
+  2: {
+    name: "Настя", 
+    lastname: "Настина", 
+    email: "nastya@mail.ru", 
+    id: 4, 
+    avatar: "https://img.freepik.com/free-psd/3d-illustration-person-with-sunglasses_23-2149436178.jpg", 
+    about: "Тут мой рассказ о себе",},
+  3: {
+    name: "Света", 
+    lastname: "Светина", 
+    email: "cveta@mail.ru", 
+    id: 5, 
+    avatar: "https://img.freepik.com/free-psd/3d-illustration-person-with-sunglasses_23-2149436178.jpg", 
+    about: "Тут мой рассказ о себе",},
+};
 
 export function getUser() {
   return user;
